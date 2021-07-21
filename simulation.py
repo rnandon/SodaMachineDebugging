33d422055f1745373e6749d4ceef05a1c67129a9
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-21 08:24:38 -0700

    added imports from appropriate files, changed will proceed to True and changed equal to operators inside while loop.

--- simulation.py
+++ simulation.py
@@ -1,4 +1,6 @@
-
+from customer import Customer
+from soda_machine import SodaMachine
+import user_interface
 
 class Simulation:
     def __init__(self):
@@ -8,14 +10,14 @@ class Simulation:
         """The central method called in main.py."""
         customer = Customer()
         soda_machine = SodaMachine()
-        will_proceed = False
+        will_proceed = True
         while will_proceed:
             user_option = user_interface.simulation_main_menu()
-            if user_option = "1":
+            if user_option == "1":
                 soda_machine.begin_transaction(customer)
-            elif user_option = "2":
+            elif user_option == "2":
                 customer.check_coins_in_wallet()
-            elif user_option = "3":
+            elif user_option == "3":
                 customer.check_backpack()
             else:
                 will_proceed = False
